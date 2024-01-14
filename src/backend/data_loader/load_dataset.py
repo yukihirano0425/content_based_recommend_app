bf3e0b78e5d4324ad1f3412efa77860b9079ba82
apple <apple@appurunoMacBook-Pro.local>
2024-01-14 18:02:20 +0900

    add:データセットの読み込み関数を追加する

--- src/backend/data_loader/load_dataset.py
+++ src/backend/data_loader/load_dataset.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
 DATA_DIR = "data/"
-
-
-def read_spend_dataset(data_dir: str, file_name: str):
-    df = pd.read_csv(f"{data_dir}{file_name}")
-    return df[df["amount"] < 0]
+TRANSACTION_COLS = ["account_id", "category_id", "date", "amount", "balance_as_of"]
+CATEGORY_COLS = ["id", "name_ja", "category_type"]
 
 
 def extract_latest_month(df: pd.DataFrame):
@@ -16,5 +13,27 @@ def extract_latest_month(df: pd.DataFrame):
     return df
 
 
-df = read_spend_dataset(DATA_DIR, "取引明細_transactions.csv")
-print(extract_latest_month(df))
+def merge_category_id(
+    spend_df: pd.DataFrame, category_df: pd.DataFrame, left_col: str, right_col: str
+):
+    return pd.merge(
+        spend_df, category_df, left_on=left_col, right_on=right_col, how="inner"
+    )
+
+
+def main(
+    file_path: str,
+    category_df_path: str,
+    category_col_for_transaction_df: str,
+    category_col_for_category_df: str,
+):
+    transaction_df = extract_latest_month(pd.read_csv(DATA_DIR + file_path))[
+        TRANSACTION_COLS
+    ]
+    spend_df = transaction_df[transaction_df["amount"] < 0]
+    return merge_category_id(
+        spend_df,
+        pd.read_csv(DATA_DIR + category_df_path)[CATEGORY_COLS],
+        category_col_for_transaction_df,
+        category_col_for_category_df,
+    )
